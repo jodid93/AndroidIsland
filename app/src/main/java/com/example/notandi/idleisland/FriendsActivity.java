ac43dd741dcfc5b8b2fb01045798c81f8bcd7d74
Mani <wowthorkell@gmail.com>
2016-03-05 14:11:57 +0000

    friends komið

--- app/src/main/java/com/example/notandi/idleisland/FriendsActivity.java
+++ app/src/main/java/com/example/notandi/idleisland/FriendsActivity.java
@@ -1,63 +1,118 @@
 package com.example.notandi.idleisland;
 
-import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
 import android.widget.Toast;
 
 /**
  * Created by thorkellmani on 26/02/16.
  */
-public class FriendsActivity extends ListActivity{
+public class FriendsActivity extends AppCompatActivity{
 
     private String UserData;
 
     private static final String UsrDat = "fokkJósúa";
 
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
-        //setListAdapter(new friendsAdapter());
 
         UserData = getIntent().getStringExtra(UsrDat);
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.friendExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        "Jósúa er Faget",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        "Þorgeir er Faget",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
-    /*private class friendsAdapter extends BaseAdapter {
-        @Override
-        public int getCount() {
-            return DummiHS.USERS.length;
-        }
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
 
-        @Override
-        public String getItem(int position) {
-            return DummiHS.USERS[position];
-        }
+        // Adding child data
+        listDataHeader.add("Pending");
+        listDataHeader.add("Friends");
 
-        @Override
-        public long getItemId(int position) {
-            return DummiHS.USERS[position].hashCode();
-        }
+        // Adding child data
+        List<String> Pending = new ArrayList<String>();
+        Pending.add("Þorgeir");
+        Pending.add("Algjör Pedo");
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup container) {
-            if (convertView == null) {
-                convertView = getLayoutInflater().inflate(R.layout.list_item, container, false);
-            }
+        List<String> Friends = new ArrayList<String>();
+        Friends.add("Jósúa");
+        Friends.add("Annar Pedo");
+        Friends.add("Þunglyndi");
 
-            ((TextView) convertView.findViewById(R.id.item))
-                    .setText(getItem(position));
-            return convertView;
-        }
-    }*/
+        listDataChild.put(listDataHeader.get(0), Pending); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), Friends);
+    }
 
     public static Intent newIntent(Context packageContext, String usrData){
         Intent i = new Intent(packageContext, FriendsActivity.class);
